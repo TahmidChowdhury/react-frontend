24d2c8834aaff8171c69063096ef91006bcc9d3e
Tahmid <tahmid.chowdhury28@gmail.com>
2019-03-15 15:17:30 -0400

    removing <a> and it's class and adding it to <Link>

--- src/core/Menu.js
+++ src/core/Menu.js
@@ -62,16 +62,13 @@ const Menu  = ({history})  =>  (
           </li>
 
           <li className="nav-item">
-            <a
-              className="nav-link"
-            >
               <Link
                 to={`/user/${isAuthenticated().user._id}`}
                 style={{color:'#fff'}}
+                className="nav-link"
               >
                 {`${isAuthenticated().user.name}'s profile`}
               </Link>
-            </a>
           </li>
         </>
       )}
