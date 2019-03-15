4acfe47c88062d4f06d6ece27de9bdaa52a035c4
Tahmid <tahmid.chowdhury28@gmail.com>
2019-03-15 00:18:42 -0400

    refactoring code from menu, signin, signup into auth/index.js

--- src/core/Menu.js
+++ src/core/Menu.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link, withRouter } from 'react-router-dom';
+import { signout, isAuthenticated } from '../auth';
 
 const isActive = (history, path) => {
   if(history.location.pathname === path)
@@ -8,32 +9,6 @@ const isActive = (history, path) => {
     return {color: "#ffffff"}
 };
 
-export const signout = (next) =>  {
-  if(typeof window !== "undefined"){
-    localStorage.removeItem("jwt");
-  }
-  next()
-  return fetch("http://localhost:8080/signout", {
-    method: "GET"
-  })
-  .then(response =>  {
-    console.log("-----user signing out-----", response);
-    return response.json();
-  })
-  .catch(err => console.log(err))
-};
-
-export const isAuthenticated = () =>  {
-  if(typeof window == "undefined")  {
-    return false;
-  }
-
-  if(localStorage.getItem("jwt")) {
-    return JSON.parse(localStorage.getItem("jwt"))
-  } else {
-    return false;
-  }
-};
 
 const Menu  = ({history})  =>  (
   <div>
